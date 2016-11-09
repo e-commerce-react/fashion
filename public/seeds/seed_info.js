9555edd7631abb676869f8259cc7b64da1c835d4
Han Hyuk Cho <han@Hans-MacBook-Air.local>
2016-11-09 12:23:25 -0500

    product seed data

---
+++ public/seeds/seed_info.js
@@ -0,0 +1,337 @@
+let male_shoe_1 = {
+  name: 'sneaker',
+  description: 'Minimalist nappa leather sneaker\nRubber sole and toecap',
+  category: 'Shoes',
+  subCategory: 'Casual',
+  gender: 'Male',
+  // "size": [5, 6, 6.5, 7, 8.5, 9, 10, 11],
+  color: 'black',
+  imageUrl: ['/seeds/male-shoe-1/male-shoe-1-1.jpg', '/seeds/male-shoe-1/male-shoe-1-2.jpg', '/seeds/male-shoe-1/male-shoe-1-3.jpg', '/seeds/male-shoe-1/male-shoe-1-4.jpg'],
+  dateAdded: new Date(),
+  price: 650,
+  stocks: 30
+};
+
+let male_shoe_2 = {
+  name: 'lace-up',
+  description: 'Opaque rubber leather and brushed calf leather laced derby shoe with wingtip motif\nCovered wedge heel with diamond-tip motif and micro sole with multicolored bands',
+  category: 'Shoes',
+  subCategory: 'Casual',
+  gender: 'Male',
+  // "size": [5, 6, 6.5, 7, 8.5, 9, 10, 11],
+  color: 'baltic blue',
+  imageUrl: ['/seeds/male-shoe-2/male-shoe-2-1.jpg', '/seeds/male-shoe-2/male-shoe-2-2.jpg', '/seeds/male-shoe-2/male-shoe-2-3.jpg', '/seeds/male-shoe-2/male-shoe-2-4.jpg'],
+  dateAdded: new Date(),
+  price: 1070,
+  stocks: 15
+};
+
+let male_shoe_3 = {
+  name: 'buckle',
+  description: 'Brushed calf leather laced derby shoe with fringe and buckle\nStitching in a contrasting color and lightened welt\nRubber peg sole',
+  category: 'Shoes',
+  subCategory: 'Dress',
+  gender: 'Male',
+  // "size": [5, 6, 6.5, 7, 8.5, 9, 10, 11],
+  color: 'white+tobacco',
+  imageUrl: ['/seeds/male-shoe-3/male-shoe-3-1.jpg', '/seeds/male-shoe-3/male-shoe-3-2.jpg', '/seeds/male-shoe-3/male-shoe-3-3.jpg', '/seeds/male-shoe-3/male-shoe-3-4.jpg'],
+  dateAdded: new Date(),
+  price: 1250,
+  stocks: 9
+};
+
+
+
+/* CHANGED CATEGORY JEWLERY => ACCESSORY */
+let male_accessory_1 = {
+  name: 'backpack',
+  description: `Python leather backpack
+Black titanium finish hardware
+Enamel triangle logo outside on the side, tag logo on the inside
+Main compartment with zipper closure
+One outside pocket with zipper closure
+One inside laptop/document pocket, one pocket with zipper closure
+Prada logo lining
+Padded back in technical fabric
+Name tag`,
+  category: 'Accessory',
+  subCategory: 'Casual', //bag, wallet, other
+  gender: 'Male',
+  // "size": [5, 6, 6.5, 7, 8.5, 9, 10, 11], // => l. 11.4    h. 15.7    w. 6.6    inches
+  color: 'black',
+  imageUrl: ['/seeds/male-accessory-1/male-accessory-1-1.jpg', '/seeds/male-accessory-1/male-accessory-1-2.jpg', '/seeds/male-accessory-1/male-accessory-1-3.jpg', '/seeds/male-accessory-1/male-accessory-4.jpg'],
+  dateAdded: new Date(),
+  price: 5169.99,
+  stocks: 9
+};
+
+let male_accessory_2 = {
+  name: 'trick',
+  description: `Calf leather diary
+Bronze-tone metal hardware
+Leather strap`,
+  category: 'Accessory',
+  subCategory: 'Casual', //bag, wallet, other
+  gender: 'Male',
+  // "size": [5, 6, 6.5, 7, 8.5, 9, 10, 11], // => l. 2.5    h. 3.1    inches
+  color: 'black',
+  imageUrl: ['/seeds/male-accessory-2/male-accessory-2-1.jpg', '/seeds/male-accessory-2/male-accessory-2-2.jpg'],
+  dateAdded: new Date(),
+  price: 480,
+  stocks: 30
+};
+
+
+//other color of male_accessory_2
+let male_accessory_3 = {
+  name: 'keychain',
+  description: `Calf leather diary
+Bronze-tone metal hardware
+Leather strap`,
+  category: 'Accessory',
+  subCategory: 'Casual', //bag, wallet, other
+  gender: 'Male',
+  // "size": [5, 6, 6.5, 7, 8.5, 9, 10, 11], // => l. 2.5    h. 3.1    inches
+  color: 'white',
+  imageUrl: ['/seeds/male-accessory-3/male-accessory-3-1.jpg', '/seeds/male-accessory-3/male-accessory-3-2.jpg'],
+  dateAdded: new Date(),
+  price: 480,
+  stocks: 45
+}
+
+let male_accessory_4 = {
+  name: 'linea rossa eyewear',
+  description: `Prada Linea Rossa Spectrum collection. Classic ultra-light pale gold metal aviator frame. Rubber inserts on temple tips. Linea Rossa logo.`,
+  category: 'Accessory',
+  subCategory: 'Casual', //bag, wallet, other
+  gender: 'Male',
+  // "size": [5, 6, 6.5, 7, 8.5, 9, 10, 11], // => l. 2.5    h. 3.1    inches
+  color: 'blue',
+  imageUrl: ['/seeds/male-accessory-4/male-accessory-4-1.jpg', '/seeds/male-accessory-4/male-accessory-4-2.jpg', '/seeds/male-accessory-4/male-accessory-4-3.jpg', '/seeds/male-accessory-4/male-accessory-4-4.jpg'],
+  dateAdded: new Date(),
+  price: 480,
+  stocks: 45
+}
+
+
+
+
+let female_bag_1 = {
+  name: 'prada bibliothèque bag',
+  description: `Prada Bibliothèque calf leather bag with bellow sides
+Double leather handle
+Detachable adjustable leather shoulder strap
+Metal hardware
+Metal lettering logo
+Snap closure on sides
+Three inside pockets, including one with zipper closure and one with flap
+Nappa leather lining`,
+  category: 'Accessory', //bag
+  subCategory: 'Dress', //bag, wallet, other
+  gender: 'Female',
+  // "size": [5, 6, 6.5, 7, 8.5, 9, 10, 11], // l. 11.8    h. 8.8    w. 5.7    inches
+  color: 'black',
+  imageUrl: ['/seeds/female-bag-1/female-bag-1-1.jpg', '/seeds/female-bag-1/female-bag-1-2.jpg', '/seeds/female-bag-1/female-bag-1-3.jpg', '/seeds/female-bag-1/female-bag-1-4.jpg'],
+  dateAdded: new Date(),
+  price: 3080,
+  stocks: 44
+};
+
+//female_ba_1 with different color
+let female_bag_2 = {
+  name: 'prada bibliothèque bag',
+  description: `Prada Bibliothèque calf leather bag with bellow sides
+Double leather handle
+Detachable adjustable leather shoulder strap
+Metal hardware
+Metal lettering logo
+Snap closure on sides
+Three inside pockets, including one with zipper closure and one with flap
+Nappa leather lining`,
+  category: 'Accessory', //bag
+  subCategory: 'Dress', //bag, wallet, other
+  gender: 'Female',
+  // "size": [5, 6, 6.5, 7, 8.5, 9, 10, 11], // l. 11.8    h. 8.8    w. 5.7    inches
+  color: 'terracotta',
+  imageUrl: ['/seeds/female-bag-2/female-bag-2-1.jpg', '/seeds/female-bag-2/female-bag-2-2.jpg', '/seeds/female-bag-2/female-bag-2-3.jpg', '/seeds/female-bag-2/female-bag-2-4.jpg'],
+  dateAdded: new Date(),
+  price: 3080,
+  stocks: 10
+};
+
+
+let female_bag_3 = {
+  name: 'prada pionnière bag',
+  description: `Detachable adjustable leather shoulder strap embellished with metal studs
+Brass hardware
+Metal lettering logo on rear
+Lace closure
+One inside pocket
+Nappa leather lining`,
+  category: 'Accessory', //bag
+  subCategory: 'Casual', //bag, wallet, other
+  gender: 'Female',
+  // "size": [5, 6, 6.5, 7, 8.5, 9, 10, 11], // l. 11.8    h. 8.8    w. 5.7    inches
+  color: 'cotto+ocra',
+  imageUrl: ['/seeds/female-bag-3/female-bag-3-1.jpg', '/seeds/female-bag-3/female-bag-3-2.jpg', '/seeds/female-bag-3/female-bag-3-3.jpg'],
+  dateAdded: new Date(),
+  price: 1940,
+  stocks: 77
+};
+
+
+let female_bag_4 = {
+  name: 'prada corsaire bag',
+  description: `Prada Corsaire quilted fabric bag
+Fabric shoulder strap
+Gold-plated hardware
+Metal lettering logo on rear
+Buckle closure
+One outside flap pocket
+One inside pocket with zipper closure
+Prada logo lining
+`,
+  category: 'Accessory', //bag
+  subCategory: 'Casual', //bag, wallet, other
+  gender: 'Female',
+  // "size": [5, 6, 6.5, 7, 8.5, 9, 10, 11], // l. 11.8    h. 8.8    w. 5.7    inches
+  color: 'camouflage+black',
+  imageUrl: ['/seeds/female-bag-4/female-bag-4-1.jpg', '/seeds/female-bag-4/female-bag-4-2.jpg', '/seeds/female-bag-4/female-bag-4-3.jpg', '/seeds/female-bag-4/female-bag-4-4.jpg'],
+  dateAdded: new Date(),
+  price: 1990,
+  stocks: 46
+};
+
+
+
+
+let female_shoe_1 = {
+    name: 'sandal',
+  description: `FW 2016 runway show
+Patent leather sandal with laminated trim
+Band and ankle strap with buckle
+Studded metal elements
+110 mm cone heel
+Leather sole
+`,
+  category: 'Shoes', //bag
+  subCategory: 'Dress', //bag, wallet, other
+  gender: 'Female',
+  // "size": [5, 6, 6.5, 7, 8.5, 9, 10, 11], // l. 11.8    h. 8.8    w. 5.7    inches
+  color: 'black+white',
+  imageUrl: ['/seeds/female-shoe-1/female-shoe-1-1.jpg', '/seeds/female-shoe-1/female-shoe-1-2.jpg', '/seeds/female-shoe-1/female-shoe-1-3.jpg', '/seeds/female-shoe-1/female-shoe-1-4.jpg'],
+  dateAdded: new Date(),
+  price: 890,
+  stocks: 55
+};
+
+
+let female_shoe_2 = {
+ name: 'pump',
+  description: `Patent leather Mary Jane pump
+Strap with contrasting trim and metal buckle
+85 mm chunky heel
+Studded metal element on the heel
+Leather sole
+`,
+  category: 'Shoes', //bag
+  subCategory: 'Casual', //bag, wallet, other
+  gender: 'Female',
+  // "size": [5, 6, 6.5, 7, 8.5, 9, 10, 11], // l. 11.8    h. 8.8    w. 5.7    inches
+  color: 'white+red',
+  imageUrl: ['/seeds/female-shoe-2/female-shoe-2-1.jpg', '/seeds/female-shoe-2/female-shoe-2-2.jpg', '/seeds/female-shoe-2/female-shoe-2-3.jpg', '/seeds/female-shoe-2/female-shoe-2-4.jpg'],
+  dateAdded: new Date(),
+  price: 850,
+  stocks: 17
+};
+
+
+let female_shoe_3 = {
+  name: 'lace-up',
+  description: `Brushed leather laced derby shoe
+Perforated wingtip motif on the toe
+55 mm leather covered wedge heel
+Wavy rubber tread sole
+`,
+  category: 'Shoes', //bag
+  subCategory: 'Casual', //bag, wallet, other
+  gender: 'Female',
+  // "size": [5, 6, 6.5, 7, 8.5, 9, 10, 11], // l. 11.8    h. 8.8    w. 5.7    inches
+  color: 'silver+royal blue',
+  imageUrl: ['/seeds/female-shoe-3/female-shoe-3-1.jpg', '/seeds/female-shoe-3/female-shoe-3-2.jpg', '/seeds/female-shoe-3/female-shoe-3-3.jpg', '/seeds/female-shoe-3/female-shoe-3-4.jpg'],
+  dateAdded: new Date(),
+  price: 950,
+  stocks: 77
+};
+
+
+let female_shoe_4 = {
+  name: 'sandal',
+  description: `FW 2016 runway show
+Gabardine bootie open on the toe and heel
+Crisscrossing laces and zipper on the inner side
+Patent leather 110 mm wedge heel with metal embellishment in relief
+Leather sole
+`,
+  category: 'Shoes', //bag
+  subCategory: 'Casual', //bag, wallet, other
+  gender: 'Female',
+  // "size": [5, 6, 6.5, 7, 8.5, 9, 10, 11], // l. 11.8    h. 8.8    w. 5.7    inches
+  color: 'black+red',
+  imageUrl: ['/seeds/female-shoe-4/female-shoe-4-1.jpg', '/seeds/female-shoe-4/female-shoe-4-2.jpg', '/seeds/female-shoe-4/female-shoe-4-3.jpg', '/seeds/female-shoe-4/female-shoe-4-4.jpg'],
+  dateAdded: new Date(),
+  price: 1450,
+  stocks: 73
+};
+
+
+let female_accessory_1 = {
+name: 'eyewear',
+  description: `Prada Cinéma. Bold tortoiseshell square acetate frame. Metal temples and bridge. Lettering logo.
+`,
+  category: 'Shoes', //bag
+  subCategory: 'Casual', //bag, wallet, other
+  gender: 'Female',
+  // "size": [5, 6, 6.5, 7, 8.5, 9, 10, 11], // l. 11.8    h. 8.8    w. 5.7    inches
+  color: 'gray+cameo pink',
+  imageUrl: ['/seeds/female-accessory-1/female-accessory-1-1.jpg', '/seeds/female-accessory-1/female-accessory-1-2.jpg', '/seeds/female-accessory-1/female-accessory-1-3.jpg', '/seeds/female-accessory-1/female-accessory-1-4.jpg'],
+  dateAdded: new Date(),
+  price: 355,
+  stocks: 120
+};
+
+let female_accessory_2 = {
+name: 'trick',
+  description: `Plexiglas key trick with metal appliqués
+Gold-plated hook and split ring
+Logo engraved on metal tag
+`,
+  category: 'Accessory', //bag
+  subCategory: 'Casual', //bag, wallet, other
+  gender: 'Female',
+  // "size": [5, 6, 6.5, 7, 8.5, 9, 10, 11], // l. 11.8    h. 8.8    w. 5.7    inches
+  color: 'amber',
+  imageUrl: ['/seeds/female-accesory-2/female-accesory-2-1.jpg', '/seeds/female-accesory-2/female-accesory-2-2.jpg'],
+  dateAdded: new Date(),
+  price: 310,
+  stocks: 100
+};
+
+
+let female_accessory_3 = {
+  name: 'trick',
+  description: `Cahier two-tone Saffiano leather trick with metal details inspired by the FW16 runway show
+Leather strap with buckle
+Small notepad inserted inside
+Leather loop and ring
+`,
+  category: 'Accessory', //bag
+  subCategory: 'Casual', //bag, wallet, other
+  gender: 'Female',
+  // "size": [5, 6, 6.5, 7, 8.5, 9, 10, 11], // l. 11.8    h. 8.8    w. 5.7    inches
+  color: 'begonia pink+black',
+  imageUrl: ['/seeds/female-accessory-3/female-accessory-3-1.jpg', '/seeds/female-accessory-3/female-accessory-3-2.jpg'],
+  dateAdded: new Date(),
+  price: 480,
+  stocks: 66
+};
